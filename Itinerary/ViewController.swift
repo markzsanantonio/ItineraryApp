3a579c7fe56567cf01ab89912b0bbad9e2594556
Mark Arjohn San Antonio <mark@workconex.com>
2019-07-18 14:06:25 +0800

    Initial Commit

---
+++ Itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Itinerary
+//
+//  Created by Mark Arjohn San Antonio on 18/07/2019.
+//  Copyright © 2019 Mark Arjohn San Antonio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
